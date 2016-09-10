1a7837a8916f01e16f5ddf685b10ce9a100a6b44
Dave <dj0hnve@gmail.com>
2016-09-10 17:06:37 -0600

    First working version with sensors.
    Arduino sketch is operational and the node cli is capturing the sensor
    values spewing out of the Arduino.

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
 #!/usr/bin/env node
-#
-# Command line client to read sensor values from Arduino
+
+// Command line client to read sensor values from Arduino
 
 "use strict"
 
 const RobotSerialInterface = require('robot-serial-iface').RobotSerialInterface
 const winston = require('winston')
 
+winston.level = process.env.LOG_LEVEL || 'info'
+
 winston.log('debug', 'Starting the robot serial interface')
 let robot = new RobotSerialInterface()
+
+robot.on('light', (light) => {
+  winston.log('debug', 'Light value: %d', light)
+})
+
+robot.on('humidity1', (h) => {
+  winston.log('debug', 'Humidity 1 value %d', h)
+})
+
+robot.on('humidity2', (h) => {
+  winston.log('debug', 'Humidity 2 value %d', h)
+})
+
+robot.on('temperature1', (t) => {
+  winston.log('debug', 'Temperature 1 value %d', t)
+})
+
+robot.on('temperature2', (t) => {
+  winston.log('debug', 'Temperature 2 value %d', t)
+})
+
+// Get the robot started 
+robot.start(
+  '/dev/ttyACM0',
+  {
+    baudrate: 9600
+  },
+  [
+    {
+      name: 'light',
+      startByte: 0x01,
+      numBytes: 2
+    },
+    {
+      name: 'humidity1',
+      startByte: 0x02,
+      numBytes: 2
+    },
+    {
+      name: 'humidity2',
+      startByte: 0x03,
+      numBytes: 2
+    },
+    {
+      name: 'temperature1',
+      startByte: 0x04,
+      numBytes: 2
+    },
+    {
+      name: 'temperature2',
+      startByte: 0x05,
+      numBytes: 2
+    }
+  ]
+)
--- sketch/src/sketch.ino
+++ sketch/src/sketch.ino
@@ -1,4 +1,5 @@
 #include "DHT.h"
+#define DEBUG 0
 
 // milliseconds of delay on the loop.
 #define DELAY 2000
@@ -6,135 +7,94 @@
 // Sensor output constants
 #define START_BYTE 0x13
 
-// #define COMMAND_PACKET_LENGTH 5
-
-// The sensor output looks like:
-// START_BYTE LEN_BYTE SENSOR_TYPE SENSOR_VALUE1 [SENSOR_VALUE2] [SENSOR_TYPE...]* CHECKSUM
-
-/*uint8_t commandPacket [COMMAND_PACKET_LENGTH];*/
-/*int currentPacketLength = 0;*/
-
-/*#define SENSOR_PACKET_LENGTH 6*/
-/*uint8_t sensorPacket [SENSOR_PACKET_LENGTH];*/
-#define ALT_SENSOR_PACKET_LENGTH 12
+#define ALT_SENSOR_PACKET_LENGTH 18
 uint8_t altSensorPacket [ALT_SENSOR_PACKET_LENGTH];
 
 #define LIGHT_SENSOR 0x01
-#define HUMIDITY_SENSOR 0x02
-#define TEMPERATURE_SENSOR 0x03
+#define HUMIDITY_SENSOR_1 0x02
+#define HUMIDITY_SENSOR_2 0x03
+#define TEMPERATURE_SENSOR_1 0x04
+#define TEMPERATURE_SENSOR_2 0x05
 
 // Sensor settings
-#define LDRPIN 0
-#define DHTPIN 4
-// #define UNIT 0 // Fahrenheit
+#define DHT_1_PIN 2 // Digital pin 2
+#define DHT_2_PIN 3 // Digital pin 3
+#define LDR_PIN 0 // Analog pin 0
 #define DHTTYPE DHT11
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht1(DHT_1_PIN, DHTTYPE);
+DHT dht2(DHT_2_PIN, DHTTYPE);
 
 void setup()
 {
   Serial.begin(9600);
 
-  // Set the digital pins to OUTPUT mode
-  /*pinMode(FORWARD, OUTPUT);*/
-  /*pinMode(BACKWARD, OUTPUT);*/
-  /*pinMode(LEFT, OUTPUT);*/
-  /*pinMode(RIGHT, OUTPUT);*/
-
-  dht.begin();
+  dht1.begin();
+  dht2.begin();
 }
 
 void loop()
 {
-  int l = analogRead(LDRPIN);
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  int l = analogRead(LDR_PIN);
 
-  float tf = t * 1.8 + 32; // Convert fromn C to F
+  float h1 = dht1.readHumidity();
+  float t1 = dht1.readTemperature();
+  float h2 = dht2.readHumidity();
+  float t2 = dht2.readTemperature();
 
-  // Send a sensor packet value
-  // sendSensorValue(PROXIMITY_SENSOR, 300);
+  float tf1 = convertFromCtoF(t1);
+  float tf2 = convertFromCtoF(t2);
 
-  // sendTempHumidValues(h, tf);
-  sendSensorValues(l, h, tf);
+#if DEBUG
+  Serial.print("Light = ");
+  Serial.println(l);
+
+  Serial.print("T1 = ");
+  Serial.println(tf1);
+  Serial.print("T2 = ");
+  Serial.println(tf2);
+
+  Serial.print("H1 = ");
+  Serial.println(h1);
+  Serial.print("H2 = ");
+  Serial.println(h2);
+#endif
+
+#if ! DEBUG
+  sendSensorValues(l, h1, tf1, h2, tf2);
+#endif
 
   delay(DELAY);
 }
 
-/*void serialEvent() {*/
-  /*while (Serial.available()) {*/
-    /*uint8_t b = Serial.read();*/
-    /*if (currentPacketLength == 0) {*/
+float convertFromCtoF(float cValue) {
+  return cValue * 1.8 + 32; // Convert fromn C to F
+}
 
-      /*if (b == DRIVE) {*/
-        /*// This is a valid command*/
-        /*commandPacket[currentPacketLength++] = b;*/
-      /*} // else skip reading until we get a valid command*/
-
-    /*} else if (currentPacketLength >= COMMAND_PACKET_LENGTH) {*/
-
-      /*// We have a complete packet; parse out the speed and*/
-      /*// direction*/
-      /*processCompletePacket();*/
-
-      /*// Clear the packet for the next command*/
-      /*currentPacketLength = 0;*/
-
-    /*} else {*/
-      /*// The packet's not full and we're collecting goodies*/
-      /*commandPacket[currentPacketLength++] = b;*/
-    /*}*/
-  /*}*/
-  /*if (currentPacketLength >= COMMAND_PACKET_LENGTH) {*/
-    /*processCompletePacket();*/
-    /*currentPacketLength = 0;*/
-  /*}*/
-/*}*/
-
-/*void processCompletePacket() {*/
-  /*int speed = commandPacket[1] << 8 | commandPacket[2];*/
-  /*int direction = commandPacket[3] << 8 | commandPacket[4];*/
-
-  /*ledsOff();*/
-
-  /*if (speed > 0) {*/
-    /*if (direction == 0) {*/
-      /*digitalWrite(FORWARD, true);*/
-    /*} else {*/
-      /*digitalWrite(LEFT, true);*/
-    /*}*/
-  /*} else if (speed < 0) {*/
-    /*// Speed is negative*/
-    /*if (direction == 0) {*/
-      /*digitalWrite(BACKWARD, true);*/
-    /*} else {*/
-      /*digitalWrite(RIGHT, true);*/
-    /*}*/
-  /*} else {*/
-    /*ledsOff(); // Same as stop*/
-  /*}*/
-
-/*}*/
-
-/*void ledsOff() {*/
-  /*digitalWrite(FORWARD, false);*/
-  /*digitalWrite(BACKWARD, false);*/
-  /*digitalWrite(LEFT, false);*/
-  /*digitalWrite(RIGHT, false);*/
-/*}*/
-
-void sendSensorValues(uint16_t light, uint16_t humidity, uint16_t temperature) {
+#if ! DEBUG
+void sendSensorValues(
+    uint16_t light, 
+    uint16_t humidity1, 
+    uint16_t temperature1, 
+    uint16_t humidity2, 
+    uint16_t temperature2) {
   altSensorPacket[0] = START_BYTE;
-  altSensorPacket[1] = 0x08;
-  altSensorPacket[2] = HUMIDITY_SENSOR;
-  altSensorPacket[3] = humidity >> 8;
-  altSensorPacket[4] = humidity & 0x00ff;
-  altSensorPacket[5] = TEMPERATURE_SENSOR;
-  altSensorPacket[6] = temperature >> 8;
-  altSensorPacket[7] = temperature & 0x00ff;
-  altSensorPacket[8] = LIGHT_SENSOR;
-  altSensorPacket[9] = light >> 8;;
-  altSensorPacket[10] = light & 0x00ff;
+  altSensorPacket[1] = 0x0f;
+  altSensorPacket[2] = HUMIDITY_SENSOR_1;
+  altSensorPacket[3] = humidity1 >> 8;
+  altSensorPacket[4] = humidity1 & 0x00ff;
+  altSensorPacket[5] = HUMIDITY_SENSOR_2;
+  altSensorPacket[6] = humidity2 >> 8;
+  altSensorPacket[7] = humidity2 & 0x00ff;
+  altSensorPacket[8] = TEMPERATURE_SENSOR_1;
+  altSensorPacket[9] = temperature1 >> 8;
+  altSensorPacket[10] = temperature1 & 0x00ff;
+  altSensorPacket[11] = TEMPERATURE_SENSOR_2;
+  altSensorPacket[12] = temperature2 >> 8;
+  altSensorPacket[13] = temperature2 & 0x00ff;
+  altSensorPacket[14] = LIGHT_SENSOR;
+  altSensorPacket[15] = light >> 8;;
+  altSensorPacket[16] = light & 0x00ff;
 
   // Calculate the checksum for the packet
   int total = 0;
@@ -142,47 +102,9 @@ void sendSensorValues(uint16_t light, uint16_t humidity, uint16_t temperature) {
     total += altSensorPacket[i];
   }
 
-  altSensorPacket[11] = -total; // Checksum
+  altSensorPacket[17] = -total; // Checksum
 
   Serial.write(altSensorPacket, ALT_SENSOR_PACKET_LENGTH);
 }
-
-/*void sendTempHumidValues(uint16_t humidity, uint16_t temperature) {*/
-  /*altSensorPacket[0] = START_BYTE;*/
-  /*altSensorPacket[1] = 0x06;*/
-  /*altSensorPacket[2] = HUMIDITY_SENSOR;*/
-  /*altSensorPacket[3] = humidity >> 8;*/
-  /*altSensorPacket[4] = humidity & 0x00ff;*/
-  /*altSensorPacket[5] = TEMPERATURE_SENSOR;*/
-  /*altSensorPacket[6] = temperature >> 8;*/
-  /*altSensorPacket[7] = temperature & 0x00ff;*/
-
-  /*// Calculate the checksum for the packet*/
-  /*int total = 0;*/
-  /*for (int i=0; i<ALT_SENSOR_PACKET_LENGTH - 1; i++) {*/
-    /*total += altSensorPacket[i];*/
-  /*}*/
-
-  /*altSensorPacket[8] = -total; // Checksum*/
-
-  /*Serial.write(altSensorPacket, ALT_SENSOR_PACKET_LENGTH);*/
-/*}*/
-
-/*void sendSensorValue(uint16_t sensor, uint16_t value) {*/
-  /*sensorPacket[0] = START_BYTE;*/
-  /*sensorPacket[1] = 0x03;*/
-  /*sensorPacket[2] = PROXIMITY_SENSOR;*/
-  /*sensorPacket[3] = value >> 8;*/
-  /*sensorPacket[4] = value & 0x00ff;*/
-
-  /*// Calculate the checksum for the packet*/
-  /*int total = 0;*/
-  /*for (int i=0; i<SENSOR_PACKET_LENGTH - 1; i++) {*/
-    /*total += sensorPacket[i];*/
-  /*}*/
-
-  /*sensorPacket[5] = -total; // Checksum*/
-
-  /*Serial.write(sensorPacket, SENSOR_PACKET_LENGTH);*/
-/*}*/
+#endif
 
