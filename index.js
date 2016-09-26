17c900f243df767cedc80d3588504d29f983f3a7
Dave <dj0hnve@gmail.com>
2016-09-26 16:38:28 -0600

    First stab at rrdtools.

--- index.js
+++ index.js
@@ -4,8 +4,14 @@
 
 "use strict"
 
+const rrd = require('rrdtools')
+const fs = require('fs')
 const RobotSerialInterface = require('robot-serial-iface').RobotSerialInterface
 const winston = require('winston')
+
+const rrdDataFile = 'readings.rrd'
+const STEP = 300
+const ENOENT = 2
 
 winston.level = process.env.LOG_LEVEL || 'info'
 
@@ -32,37 +38,81 @@ robot.on('temperature2', (t) => {
   winston.log('debug', 'Temperature 2 value %d', t)
 })
 
-// Get the robot started 
-robot.start(
-  '/dev/ttyACM0',
-  {
-    baudrate: 9600
-  },
-  [
-    {
-      name: 'light',
-      startByte: 0x01,
-      numBytes: 2
-    },
-    {
-      name: 'humidity1',
-      startByte: 0x02,
-      numBytes: 2
-    },
-    {
-      name: 'humidity2',
-      startByte: 0x03,
-      numBytes: 2
-    },
-    {
-      name: 'temperature1',
-      startByte: 0x04,
-      numBytes: 2
-    },
-    {
-      name: 'temperature2',
-      startByte: 0x05,
-      numBytes: 2
-    }
-  ]
-)
+function createRRDFile(fileName) {
+  return new Promise((resolve, reject) => {
+    rrd.create(
+      rrdDataFile,
+      STEP,
+      now,
+      [
+       'DS:inside-temp:GAUGE:600:-35:120',
+       'DS:outside-temp:GAUGE:600:-35:120',
+       'DS:inside-humidity:GAUGE:600:0:100',
+       'DS:outside-humidity:GAUGE:600:0:100',
+       'DS:inside-light:GAUGE:600:0:1000',
+       'RRA:MIN:0.5:12:1440',
+       'RRA:MAX:0.5:12:1440',
+       'RRA:AVERAGE:0.5:1:1440'
+     ], 
+     (err) => {
+       if (err) return reject(err)
+       resolve()
+     })
+  })
+}
+
+function ensureRRDFileExists(fileName) {
+  return new Promise((resolve, reject) => {
+    fs.stat(fileName, (err, stat) => {
+      if (err) {
+        if (err.code === ENOENT) {
+          createRRDFile(fileName)
+            .then(resolve)
+            .catch(reject)
+        } else {
+          reject(err)
+        }
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+ensureRRDFileExists(rrdDataFile)
+  .then(() => {
+    // Get the robot started 
+    robot.start(
+      '/dev/ttyACM0',
+      {
+        baudrate: 9600
+      },
+      [
+        {
+          name: 'light',
+          startByte: 0x01,
+          numBytes: 2
+        },
+        {
+          name: 'humidity1',
+          startByte: 0x02,
+          numBytes: 2
+        },
+        {
+          name: 'humidity2',
+          startByte: 0x03,
+          numBytes: 2
+        },
+        {
+          name: 'temperature1',
+          startByte: 0x04,
+          numBytes: 2
+        },
+        {
+          name: 'temperature2',
+          startByte: 0x05,
+          numBytes: 2
+        }
+      ]
+    )
+  })
